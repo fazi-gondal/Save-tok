310159a7bd80831070222f18fa98c3d9e9f9c25e
fazi-gondal <nextinpk@gamil.com>
2024-04-14 01:27:26 -0700

    update

--- lib/widgets/home/guide_widget.dart
+++ lib/widgets/home/guide_widget.dart
@@ -9,53 +9,55 @@ class GuideWidget extends StatelessWidget {
     return Expanded(
       child: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const Text(
-              "How to download?",
-              style: TextStyle(fontWeight: FontWeight.bold),
+              "TikTok Video Downloader By Fazi💗",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                textAlign: TextAlign.center,
+              ),
             ),
             const SizedBox(
               height: 5.0,
             ),
             const Steps(
-              no: "1",
-              message:
-                  "Open tiktok app and find the video that you want to download, and click on share.",
+              message: "Download TikTok Videos Without Watermark",
             ),
-            Image.asset("assets/step_1.png"),
-            const SizedBox(
-              height: 8.0,
-            ),
-            const Steps(
-              no: "2",
-              message: "Click on copy link and close the tiktok app.",
-            ),
-            Image.asset("assets/step_2.png"),
-            const SizedBox(
-              height: 8.0,
-            ),
-            const Steps(
-              no: "3",
-              message:
-                  "Open SaveTok app and paste the tiktok video link you copied.",
-            ),
-            Image.asset("assets/step_3.png"),
-            const SizedBox(
-              height: 8.0,
-            ),
-            const Steps(
-              no: "4",
-              message: "Wait for the application to fetch the video info.",
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            const Steps(
-              no: "5",
-              message: "Click the download button to start downloading.",
-            ),
-            Image.asset("assets/step_5.png"),
+            // // Image.asset("assets/step_1.jpeg"),
+            // const SizedBox(
+            //   height: 5.0,
+            // ),
+            // const Steps(
+            //   no: "2",
+            //   message: "Click on copy link and close the tiktok app.",
+            // ),
+            // // Image.asset("assets/step_2.png"),
+            // const SizedBox(
+            //   height: 5.0,
+            // ),
+            // const Steps(
+            //   no: "3",
+            //   message:
+            //       "Open SaveTok app and paste the tiktok video link you copied.",
+            // ),
+            // // Image.asset("assets/step_3.png"),
+            // const SizedBox(
+            //   height: 5.0,
+            // ),
+            // const Steps(
+            //   no: "4",
+            //   message: "Wait for the application to fetch the video info.",
+            // ),
+            // const SizedBox(
+            //   height: 5.0,
+            // ),
+            // const Steps(
+            //   no: "5",
+            //   message: "Click the download button to start downloading.",
+            // ),
+            //Image.asset("assets/about.png"),
           ],
         ),
       ),
